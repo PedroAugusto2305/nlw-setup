ee1bce34e9cfbbc800546b4ffe8d39d7ed80604f
Pedro Augusto <p.augusto_23@outlook.com>
2023-01-16 22:51:22 -0300

    :wrench: Configurando ambiente do server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,16 @@
 import fastify from "fastify";
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
 
 const app = fastify()
+const prisma = new PrismaClient()
 
-app.get('/', () => {
-  return 'Hello NLW'
+app.register(cors)
+
+app.get('/hello', async () => {
+  const habits = await prisma.habit.findMany()
+
+  return habits
 })
 
-app.listen({port: 3333,})
+app.listen({port: 3333,}).then(() => {console.log('Server Running')})
